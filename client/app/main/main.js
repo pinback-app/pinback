a4b29d3548ca7b6b7529981c172e71cebab9eda1
bbalaran <balaran202@gmail.com>
2016-02-16 22:18:17 -0800

    filter not working, basemap not rendering

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -19,68 +19,97 @@ angular.module('greenfield.main', ['leaflet-directive'])
       $scope.addEvent = function(event) {
           main.eventRequest(event);
         }
-          //set data to bandsintown content
-        var data = $location.search();
-        //declare map maprkers
-        $scope.markers = [];
-        $log.log($scope.markers)
-        for (var i = 0; i < data.mapData.data.length; i++) {
-          marker = {
-            id: i,
-            name: data.mapData.data[i].name,
-            lat: data.mapData.data[i].latitude,
-            lng: data.mapData.data[i].longitude,
-            events: data.mapData.data[i].events,
-            message: data.mapData.data[i].name
+        //set data to bandsintown content
+      var data = $location.search();
+      //declare map markers
+      $scope.markers = [];
+      $log.log($scope.markers)
+      for (var i = 0; i < data.mapData.data.length; i++) {
+        marker = {
+          id: i,
+          name: data.mapData.data[i].name,
+          lat: data.mapData.data[i].latitude,
+          lng: data.mapData.data[i].longitude,
+          events: data.mapData.data[i].events,
+          message: data.mapData.data[i].name
+        }
+        $scope.markers.push(marker)
+      };
+
+      $scope.$watchCollection("search",
+        function(newValue, oldValue) {
+
+          if (newValue === oldValue) {
+            return;
           }
-          $scope.markers.push(marker)
-        };
+          var data = angular.copy($scope.markers);
 
-        //Set data variables for rendering venue information on click
-        //"Could not load data" should not be displayed
-        $scope.data = {};
-        $scope.data.showMarker = {
-          name: 'Could not load data'
-        };
-        $scope.data.venue = 'Could not load data';
+          var justGroup = _.filter(data.features, function(x) {
 
-        //extend scope to map objects and set defaults
-        angular.extend($scope, {
-          defaults: {
-            minZoom: 11,
-            scrollWheelZoom: false
-          },
-          center: {
-            lat: $scope.markers[0].lat,
-            lng: $scope.markers[0].lng,
-            zoom: 12
-          },
-          markers: $scope.markers,
-          position: {
-            lat: $scope.markers[0].lat,
-            lng: $scope.markers[0].lng
-          },
-          layers: {
-            baselayers: {
-              osm: {
-                name: 'CartoDB',
-                url: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
-                type: 'xyz'
+              if (!newValue.name) {
+                return true
+              } else {
+                return $filter('filter')([x.properties.name], newValue.name).length > 0
               }
-            }
+
+          })
+
+          data.features = justGroup
+          $scope.geojson = {
+            data: data,
+            resetStyleOnMouseout: true
           }
-        });
+
+        }
+      );
 
 
 
-        //displays event information when marker is clicked
-        $scope.$on('leafletDirectiveMarker.click', function(e, args) {
-          // references data by id in args
-          var id = args.leafletEvent.target.options.id;
-          var venue = $scope.markers[id];
-          main.venueRequest(venue);
-          $scope.data.showMarker = $scope.markers[id].events;
-          $scope.reveal = true;
-          // console.log($scope.data.showMarker[0])
-        });
-  })
+      // //Set data variables for rendering venue information on click
+      // //"Could not load data" should not be displayed
+      // $scope.data = {};
+      // $scope.data.showMarker = {
+      //   name: 'Could not load data'
+      // };
+      // $scope.data.venue = 'Could not load data';
+
+      // //extend scope to map objects and set defaults
+      // angular.extend($scope, {
+      //   defaults: {
+      //     minZoom: 11,
+      //     scrollWheelZoom: false
+      //   },
+      //   center: {
+      //     lat: $scope.markers[0].lat,
+      //     lng: $scope.markers[0].lng,
+      //     zoom: 12
+      //   },
+      //   markers: $scope.markers,
+      //   position: {
+      //     lat: $scope.markers[0].lat,
+      //     lng: $scope.markers[0].lng
+      //   },
+      //   layers: {
+      //     baselayers: {
+      //       osm: {
+      //         name: 'CartoDB',
+      //         url: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+      //         type: 'xyz'
+      //       }
+      //     }
+      //   }
+      // });
+
+
+
+      //       //displays event information when marker is clicked
+      //       $scope.$on('leafletDirectiveMarker.click', function(e, args) {
+      //         // references data by id in args
+      //         var id = args.leafletEvent.target.options.id;
+      //         var venue = $scope.markers[id];
+      //         main.venueRequest(venue);
+      //         $scope.data.showMarker = $scope.markers[id].events;
+      //         $scope.reveal = true;
+      //         // console.log($scope.data.showMarker[0])
+      //       });
+      })
